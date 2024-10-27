a19ea4c5846be551baceea072c3112b4e3c13aac
Asad-Ali <selcouth683@gmail.com>
2024-10-28 03:42:34 +0500

    status: new component (Mycalc), routing configured. Added a component called mycalc and configured routing. wrote basic html layout for the calculator.

---
+++ calculator/src/app/mycalc/mycalc.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mycalc',
+  standalone: true,
+  imports: [],
+  templateUrl: './mycalc.component.html',
+  styleUrl: './mycalc.component.css'
+})
+export class MycalcComponent {
+
+}
