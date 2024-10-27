b5450e0c54db5011b06d126553b63a97a873330b
Asad-Ali <selcouth683@gmail.com>
2024-10-28 04:29:59 +0500

    made input form temporarily read-only so I can implement buttons functionality accurately.

--- calculator/src/app/mycalc/mycalc.component.ts
+++ calculator/src/app/mycalc/mycalc.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-mycalc',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './mycalc.component.html',
   styleUrl: './mycalc.component.css'
 })
 export class MycalcComponent {
+  currentTheme: string = 'default';
+  displayValue: string = '0';
 
+  setTheme(theme: string) {
+    this.currentTheme=theme;
+  }
 }
