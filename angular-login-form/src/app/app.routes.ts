0c38f5c4e82a987a1084605e667868bf72d7c282
Asad-Ali <selcouth683@gmail.com>
2024-10-28 03:42:02 +0500

    Routing configured and login forms are working fine.

--- angular-login-form/src/app/app.routes.ts
+++ angular-login-form/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import {LoginComponent} from './components/login/login.component';
+import {SignupComponent} from './components/signup/signup.component';
+import {ForgottenpassComponent} from './components/forgottenpass/forgottenpass.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'', redirectTo: 'login', pathMatch: 'full' },
+  {path:'login', component: LoginComponent},
+  {path: 'signup', component: SignupComponent},
+  {path: 'forgottenpass', component: ForgottenpassComponent},
+  {path: '**', redirectTo: 'login', pathMatch: 'full'}
+];
